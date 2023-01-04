219d64d56d883cc695932bbc44387e226b936ffa
j-s-suriyakumar <107878071+j-s-suriyakumar@users.noreply.github.com>
2023-01-04 20:31:37 +0530

    Updating UHC provider references extraction

--- uhc_provider_references_extraction.py
+++ uhc_provider_references_extraction.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan  2 12:43:22 2023
+Created on Wed Jan  4 14:37:02 2023
 
-@author: suriya
+@author: HospDataScrap
 """
 
 
@@ -26,15 +26,24 @@ class DecimalEncoder(json.JSONEncoder):
     
 
 
-df = pd.read_excel(r"C:\Users\suriya\Downloads\jan_uhc_in_network_links_priority.xlsx")
+df = pd.read_excel(r"D:\Users\HospDataScrap\Downloads\jan_uhc_in_network_links_priority.xlsx")
 
-os.chdir(r"C:\Users\suriya\Downloads\1")
+os.chdir(r"D:\Users\HospDataScrap\Downloads\2")
 session = boto3.Session(aws_access_key_id='access key',aws_secret_access_key='secret key')
-for url in tqdm(df["s3_uri"].iloc[898:901]):
+for url in tqdm(df["s3_uri"].iloc[901:910]):
     fname = url.split('/')[-1].replace('.json','')
+    df1 = pd.read_excel(r"D:\Users\HospDataScrap\Downloads\jan_payers\uhc\uhc_pro_ref_count_with_fname(901-1000).csv")
+    for index, row in df1.iterrows():
+        if fname == row['file_name']:
+            # print(fname)
+            # print(row['file_name'])
+            n_value = row['n-count']
     with open(url, 'rb', transport_params={'client': session.client('s3')}) as fout:
         objects = ijson.items(fout, 'provider_references.item')
         for n, obj in enumerate(objects):
+            if n == n_value:
+                break
+            
             with open(f'{n}.json','w') as f:
                 json.dump(obj,f, cls = DecimalEncoder)
             s3.meta.client.upload_file(f'{n}.json', 'zigna-nsa-data',f'provider-references/Q1-2023/jan-uhc/{fname}/{n}.json')
