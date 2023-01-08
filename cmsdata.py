d52ddc5bc9f59be6e130faab0db57aae085a3b39
j-s-suriyakumar <suriya@zignaai.com>
2023-01-08 17:49:11 +0530

    update cms data

--- cmsdata.py
+++ cmsdata.py
@@ -25,6 +25,8 @@ def  cmsdata(path, indices, columns, **kwargs):
         df.loc[:,columns[c]] = d
         c+=1
     
+    df.drop(columns = 'Filler',inplace = True)
+    
     if out_path:
         fname = path.split('\\')[-1].replace('.text','.csv')
         op = out_path + '\\' + fname
@@ -35,15 +37,15 @@ def  cmsdata(path, indices, columns, **kwargs):
     
     print('Preview of the data:')
     print(df.head())
-    df.drop(columns = 'Filler',inplace = True)
+    
     return df
 
-path = r"C:\Users\User\Downloads\PFALL23.txt"
+# path = r"C:\Users\User\Downloads\PFALL23.txt"
 
-columns = ['Filler',	'Year',	'Filler',	'Carrier Number',	'Filler',	'Locality',	'Filler',	'HCPCS Code',	'Filler',	'Modifier',	'Filler',	'Non Facility Fee Schedule Amount',	'Filler',	'Facility Fee Schedule Amount',	'Filler',	'Filler',	'Filler',	'PCTC Indicator',	'Filler',	'Status Code', 'Filler',	'Multiple Surgery Indicator',	'Filler',	'50% Therapy Reduction Amount','Filler',	'50% Therapy Reduction Amount',	'Filler',	'OPPS Indicator',	'Filler',	'OPPS Non Facility Fee Amount',	'Filler',	'OPPS Faclility Fee Amount',	'Filler']
+# columns = ['Filler',	'Year',	'Filler',	'Carrier Number',	'Filler',	'Locality',	'Filler',	'HCPCS Code',	'Filler',	'Modifier',	'Filler',	'Non Facility Fee Schedule Amount',	'Filler',	'Facility Fee Schedule Amount',	'Filler',	'Filler',	'Filler',	'PCTC Indicator',	'Filler',	'Status Code', 'Filler',	'Multiple Surgery Indicator',	'Filler',	'50% Therapy Reduction Amount','Filler',	'50% Therapy Reduction Amount',	'Filler',	'OPPS Indicator',	'Filler',	'OPPS Non Facility Fee Amount',	'Filler',	'OPPS Faclility Fee Amount',	'Filler']
 
-indices = [(1, 1), (2, 5), (6, 8), (9, 13),(14,16),(17,18),(19,21),(22,26),(27,29),(30,31),(32,34),(35,44),(45,47),(48,57),(58,60),(61,61),(62,64),(65,65),(66,68),(69,69),(70,72),(73,73),(74,76),(77,86),(87,89),(90,99),(100,102),(103,103),(104,106),(107,116),(117,119),(120,129),(130,130)]
+# indices = [(1, 1), (2, 5), (6, 8), (9, 13),(14,16),(17,18),(19,21),(22,26),(27,29),(30,31),(32,34),(35,44),(45,47),(48,57),(58,60),(61,61),(62,64),(65,65),(66,68),(69,69),(70,72),(73,73),(74,76),(77,86),(87,89),(90,99),(100,102),(103,103),(104,106),(107,116),(117,119),(120,129),(130,130)]
 
 
-data = cmsdata(path, indices, columns, outdir = r"C:\Users\User\Downloads")
+# data = cmsdata(path, indices, columns, outdir = r"C:\Users\User\Downloads")
 
